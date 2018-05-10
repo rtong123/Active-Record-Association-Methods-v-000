3f129771465de6715934d08106f8898fa32bd8e4
Rebecca Tong <rtong@buffalo.edu>
2018-05-10 18:32:52 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,8 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artist.name
+    self.songs.collect do |song|
+      song.artist.name
+    end
   end
 end
