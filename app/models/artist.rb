69510f50037a785ba223a4049fd468920ca7faff
Rebecca Tong <rtong@buffalo.edu>
2018-05-10 18:23:58 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,9 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
-  def get_genre_of_first_song
-    self.songs.first
-  end
+  def get_first_song
+     self.songs.first
+   end
 
   def song_count
     #return the number of songs associated with the artist
